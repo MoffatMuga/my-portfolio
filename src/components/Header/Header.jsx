f11b579adce66b6743ae5a79ae63365974e558e4
Moffat <moffat.muga@gmail.com>
2023-11-08 11:32:52 +0300

    style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,6 +12,29 @@ function Header() {
                     <p className='text-smallTextColor text-[12px] font-[500]'>personal</p>
                 </div>
             </div>
+
+            <div className="menu">
+                <ul className='flex items-center gap-[10px]'>
+                    <li>
+                        <a className= 'text-smallTextColor font-[600]'href="#about">About</a>
+                    </li>
+                    <li>
+                        <a className= 'text-smallTextColor font-[600]' href="#experience">Experience</a>
+                    </li>
+                    <li>
+                        <a className= 'text-smallTextColor font-[600]' href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a className= 'text-smallTextColor font-[600]' href="#contact">contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='flex items-center gap-4'>
+                <button className='flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] '>
+                    <i class="ri-send-plane-line"></i> let's talk
+                </button>
+                <span className='md:hidden text-smallTextColor text-2xl cursor-pointer'><i class="ri-menu-4-fill"></i></span>
+            </div>
         </div>
     </div>
     </header>
