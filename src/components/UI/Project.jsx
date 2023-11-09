b6592c0348ccdec248058a521c99934adc296489
Moffat <moffat.muga@gmail.com>
2023-11-09 10:02:59 +0300

    add projects

--- src/components/UI/Project.jsx
+++ src/components/UI/Project.jsx
@@ -1,7 +1,14 @@
 import data from "../../assets/data/portfolioData";
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Project() {
+
+    const [nextItems, setNextItems] = useState(6)
+    const [portfolios, setPortfolios] = useState(data)
+
+    const seeMoreHandler = () =>{
+        setNextItems(prev => prev + 3)
+    }
   return (
     <section id='projects'>
         <div className='container'>
@@ -13,14 +20,32 @@ function Project() {
 
             <div className='flex items-center gap-4 flex-wrap mt-12'> 
                     {
-                        data?.map((portfolio,index) =>(
-                            <div className='group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]'>
+                        portfolios.slice(0, nextItems)?.map((portfolio,index) =>(
+                            <div key={index}
+                            data-aos='zoom-in' data-aos-delay='50' data-aos-duration='1000'className='group max-w-full sm:w-[48.5%] md:w-[31.8%] lg:w-[32.2%] relative z-[1]'>
                                 <figure>
                                     <img className='rounded-[8px] 'src={portfolio.imgUrl} alt="" />
                                 </figure>
+
+                                <div className="w-full h-full bg-primaryColor bg-opacity-40 absolute top-0 left-0 z-5 hidden group-hover:block ">
+                                    <div className="flex items-center justify-center w-full h-full">
+                                        <button className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-2 rounded-[8px] font-[500] ease-in duration-200 ">
+                                            Details
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                         ))
                     }
+            </div>
+
+            <div className="text-center mt-6">
+                                    <div onClick={seeMoreHandler}
+                                    className="flex items-center justify-center w-full h-full">
+                                        <button className="text-white bg-headingColor hover:bg-smallTextColor py-2 px-2 rounded-[8px] font-[500] ease-in duration-200 ">
+                                            see more
+                                        </button>
+                                    </div>
             </div>
 
         </div>
